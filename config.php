cde7c8fcf3fe4d12fcd52374db107d6de7332d65
baiwulin <i@baiwulin.com>
2023-11-27 22:42:06 +0800

    移除 过时/不常用 登录方式的配置项

--- config.php
+++ config.php
@@ -2,54 +2,19 @@
 //定义回调URL通用的URL
 define('URL_CALLBACK', Typecho_Common::url('/oauth_callback?type=', Typecho_Widget::Widget('Widget_Options')->index));
 return array(
-    //腾讯QQ登录配置
+    //QQ配置
     'THINK_SDK_QQ'      => array(
-        'NAME'      => '腾讯QQ',
+        'NAME'      => 'QQ',
         'APP_KEY'       => '', //应用注册成功后分配的 APP ID
         'APP_SECRET'    => '', //应用注册成功后分配的KEY
         'CALLBACK'      => URL_CALLBACK . 'qq',
     ),
-    //腾讯微博配置
-    'THINK_SDK_TENCENT' => array(
-        'NAME'      => '腾讯微博',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'tencent',
-    ),
-    //新浪微博配置
+    //微博配置
     'THINK_SDK_SINA'    => array(
         'NAME'      => '新浪微博',
         'APP_KEY'       => '', //应用注册成功后分配的 APP ID
         'APP_SECRET'    => '', //应用注册成功后分配的KEY
         'CALLBACK'      => URL_CALLBACK . 'sina',
-    ),
-    //网易微博配置
-    'THINK_SDK_T163'    => array(
-        'NAME'      => '网易微博',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 't163',
-    ),
-    //人人网配置
-    'THINK_SDK_RENREN'  => array(
-        'NAME'      => '人人网',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'renren',
-    ),
-    //360配置
-    'THINK_SDK_X360'    => array(
-        'NAME'      => '360',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'x360',
-    ),
-    //豆瓣配置
-    'THINK_SDK_DOUBAN'  => array(
-        'NAME'      => '豆瓣',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'douban',
     ),
     //Github配置
     'THINK_SDK_GITHUB'  => array(
@@ -65,26 +30,12 @@ return array(
         'APP_SECRET'    => '', //应用注册成功后分配的KEY
         'CALLBACK'      => URL_CALLBACK . 'google',
     ),
-    //MSN配置
+    //微软配置
     'THINK_SDK_MSN'     => array(
         'NAME'      => 'MSN',
         'APP_KEY'       => '', //应用注册成功后分配的 APP ID
         'APP_SECRET'    => '', //应用注册成功后分配的KEY
         'CALLBACK'      => URL_CALLBACK . 'msn',
-    ),
-    //点点配置
-    'THINK_SDK_DIANDIAN'=> array(
-        'NAME'      => '点点',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'diandian',
-    ),
-    //淘宝网配置
-    'THINK_SDK_TAOBAO'  => array(
-        'NAME'      => '淘宝网',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'taobao',
     ),
     //百度配置
     'THINK_SDK_BAIDU'   => array(
@@ -92,19 +43,5 @@ return array(
         'APP_KEY'       => '', //应用注册成功后分配的 APP ID
         'APP_SECRET'    => '', //应用注册成功后分配的KEY
         'CALLBACK'      => URL_CALLBACK . 'baidu',
-    ),
-    //开心网配置
-    'THINK_SDK_KAIXIN'  => array(
-        'NAME'      => '开心网',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'kaixin',
-    ),
-    //搜狐微博配置
-    'THINK_SDK_SOHU'    => array(
-        'NAME'      => '搜狐微博',
-        'APP_KEY'       => '', //应用注册成功后分配的 APP ID
-        'APP_SECRET'    => '', //应用注册成功后分配的KEY
-        'CALLBACK'      => URL_CALLBACK . 'sohu',
-    ),
+    )
 );
